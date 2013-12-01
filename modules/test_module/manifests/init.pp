3f3bd390a0f1a43766ed50bbe24ac81e8d19bfff
GS <gs@github.com>
2013-12-01 21:27:48 +0000

    added the offline key/value pair.
    note - variables need to be declared with values at the top of the class

--- modules/test_module/manifests/init.pp
+++ modules/test_module/manifests/init.pp
@@ -1,20 +1,17 @@
 # test module which places a file
 
-class test_module ($testvar = 'default text') {
+class test_module (
+  $testvar = 'default text',
+  $offline = 'test',
+) {
+
+  unless $offline == offline {
 
     file { 'test1':
         ensure  => file,
         path    => '/var/tmp/test1file',
         content => $testvar,
   }
-
-  #    file { 'test2':
-  #        ensure  => 'present',
-  #        path    => "/var/tmp/${::test_module::testvar}",
-  #        content => hiera($testvar),
-  #         content => "contents of testvar ${::test_module::testvar}",
-  #  }
-
 
     notice ("this is the fully qualified notice line......${test_module::testvar} blah")
     notice ("this is the notice line......${testvar} blah")
@@ -28,4 +25,5 @@ class test_module ($testvar = 'default text') {
       withpath => false,
       name     => " this is the notify line........ $testvar blah",
     }
+  }
 }
