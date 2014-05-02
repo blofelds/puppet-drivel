315b07fbf56f1e1e9c36ecaf171be9fc2147d62f
GS <gs@github.com>
2014-05-02 09:28:26 +0100

    add test for existence and absence of $update var

--- modules/ntp/spec/classes/config_spec.rb
+++ modules/ntp/spec/classes/config_spec.rb
@@ -17,18 +17,36 @@ describe 'ntp' do
       )
     end
 
-    context 'with $infra_server set to 0.0.0.0' do
-      let(:params) {{ :infraserver  => '0.0.0.0',
-                      :update       => 'true' }}
+    context 'when $update is defined it' do
+      let(:params) {{ :update => 'yes' }}
 
+      it 'should execute the ntpdate command' do
+        should contain_exec('update date')
+      end
+    end
+
+    context 'when $update is not defined it' do
+      let(:params) {{ :update => '' }}
+
+      it 'should not execute the ntpdate command' do
+        should_not contain_exec('update date')
+      end
+    end
+
+    context 'when $update is defined and $infra_server set to 0.0.0.0' do
+      let(:params) {{ :infraserver  => '0.0.0.0',
+                      :update       => '1' }}
+
+   # this test needs to be fixed
       it 'should execute the ntpdate command' do
         should contain_exec('update date').with(
           'require' => 'File[ntp.conf]',
    #      'command' => '/usr/sbin/ntpdate ${infra_server}',
+   #      'command' => '/usr/sbin/ntpdate 0.0.0.0',
           'onlyif'  => '/etc/init.d/ntpd status | grep stopped',
       )  
       end
     end
+
   end
-# need to add some lets to describe the parameters
 end
