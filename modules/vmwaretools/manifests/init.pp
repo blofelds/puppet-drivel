00cbb13f1de28cf72ca5b155ac7da4643a4a997a
GS <gs@github.com>
2014-05-07 09:17:21 +0100

    i really don't know what's going on here

---
+++ modules/vmwaretools/manifests/init.pp
@@ -0,0 +1,11 @@
+# installs vmware adn restarts service
+class vmwaretools (
+$service_ensure = 'running',
+) {
+
+  anchor { 'vmwaretools::begin': } ~>
+  class { 'vmwaretools::package': } ~>
+  class { 'vmwaretools::service': } ~>
+  anchor { 'vmwaretools::end': }
+
+}
