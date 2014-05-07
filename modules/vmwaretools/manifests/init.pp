73b8b7fa69b1bf11a78d7e0c48dda8878da046a7
GS <gs@github.com>
2014-05-07 08:33:52 +0100

    add service_ensure parameter to vmware-tools.again

--- modules/vmwaretools/manifests/init.pp
+++ modules/vmwaretools/manifests/init.pp
@@ -1,11 +1,11 @@
-# installs vmware adn restarts service
+# installs vmware and restarts service
 class vmwaretools (
 $service_ensure = 'running',
 ) {
 
-  anchor { 'vmwaretools::begin': } ~>
-  class { 'vmwaretools::package': } ~>
-  class { 'vmwaretools::service': } ~>
+  anchor { 'vmwaretools::begin': } ->
+  class { 'vmwaretools::package': } ->
+  class { 'vmwaretools::service': } ->
   anchor { 'vmwaretools::end': }
 
 }
