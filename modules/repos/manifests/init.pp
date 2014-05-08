5c19376dfa4acc4e62f43958325d7e9fb68af342
GS <gs@github.com>
2014-05-08 08:55:39 +0100

    FIX CYCLICAL DEPENDENCY ERRORS IN REPOS CLASS
    putting the purge first ensures that the auto-required folder is in place before placing the
    files that auto-require it. the purge can take place before the files are placed because
    puppet knows that it manages the file before it actually places it.
    
    TODO create a proper rspec fixture to enable the tests to find the hiera files

--- modules/repos/manifests/init.pp
+++ modules/repos/manifests/init.pp
@@ -1,10 +1,10 @@
 # sets class-wide variables for use in scope
 class repos  {
 
-  anchor { 'repos::begin': }
-  class { 'repos::create': }
-  class { 'repos::files': }
-  class { 'repos::purge': } 
+  anchor { 'repos::begin': } ->
+  class { 'repos::purge': }  ->
+  class { 'repos::create': } ->
+  class { 'repos::files': } ->
   anchor { 'repos::end': }
 
 }
