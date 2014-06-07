e0ac51e879da83b83eabad1bc47096998c9e44fa
GS <gs@github.com>
2014-06-08 00:58:57 +0100

    temporarily set source/dest ip's to params

--- modules/firewall/lib/puppet/type/linux_firewall.rb
+++ modules/firewall/lib/puppet/type/linux_firewall.rb
@@ -5,26 +5,42 @@
 Puppet::Type.newtype(:linux_firewall) do
   @doc = "Input firewall rules. Currently only iptables"
 
-  ensurable
+  ensurable do
+    defaultto(:present)
+    newvalue(:present) do
+      provider.create
+    end
+    newvalue(:absent) do
+      provider.destroy
+    end
+  end
 
   newparam(:name, :namevar => :true) do
 # this should probably be a property instead of a param
     desc "The name of the rule - stored in the rules comment field"
-    munge do |value|
-      value.downcase
-    end
-    def insync?(is)
-      is.downcase == should.downcase
-    end
   end
 
-  newproperty(:source_ip) do
+  newparam(:source_ip, :array_matching => :all) do
     desc "The source ip"
   end
 
-  newproperty(:dest_ip) do
+  newparam(:dest_ip) do
     desc "The dest_ip"
   end
 
 # prolly need to define "insync?"
 end
+
+
+
+
+# CREATE A "LIST" DEFINITION WHICH CONTAINS THE WHOLE LIST:- LIKE THE FOLLOWING
+# this 'array matching all' could be used for the getter/setter
+  def domains
+    get_proxy_bypass_domains(resource[:name])
+  end
+
+  def domains=(value)
+    networksetup(['-setproxybypassdomains', resource[:name], value])
+  end
+
