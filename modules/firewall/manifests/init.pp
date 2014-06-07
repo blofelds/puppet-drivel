c92276aab5310b2d5330cf9a90bd83728c2d9838
GS <gs@github.com>
2014-06-08 00:50:48 +0100

    USE CIDR IP ADDRESSES

--- modules/firewall/manifests/init.pp
+++ modules/firewall/manifests/init.pp
@@ -2,8 +2,8 @@ class firewall {
 
   linux_firewall { 'resourceinstance1':
     name => 'testname',
-#    ensure => 'present',
-    dest_ip => '10.2.2.2',
-    source_ip => '10.2.3.4',
+    ensure => 'present',
+    dest_ip => '10.2.2.2/32',
+    source_ip => '10.2.3.4/32',
   }
 }
