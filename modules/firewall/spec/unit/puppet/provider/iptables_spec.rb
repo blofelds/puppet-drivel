972dfdf40a65aa96ae80ad41df93cb7714e39901
GS <gs@github.com>
2014-06-13 03:52:00 +0100

    BEGIN FILLING OUT TESTS. ADD TESTS FOR TYPE
    need to complete basic tests for provider

--- modules/firewall/spec/unit/puppet/provider/iptables_spec.rb
+++ modules/firewall/spec/unit/puppet/provider/iptables_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
-describe 'iptables' do
+describe Puppet::Type.type(:linux_firewall).provider(:iptables) do
+
+  let(:resource) do
+    Puppet::Type.type(:linux_firewall).new(
+      :name => 'rule1',
+      :source_ip => '2.3.4.5/32',
+      :dest_ip => '3.4.5.6/32',
+    )
+  end
+
+  describe 'get_rules_list' do
+    it do
+    end
+  end
+
+  describe 'exists?' do
+    it do
+    end
+  end
+
+  describe 'destroy' do
+    it do
+    end
+  end
+
+  describe 'create' do
+    it do
+    end
+  end
+
 end
--- modules/firewall/spec/unit/puppet/type/linux_firewall_spec.rb
+++ modules/firewall/spec/unit/puppet/type/linux_firewall_spec.rb
@@ -1,4 +1,38 @@
 require 'spec_helper'
 
-describe 'linux_firewall' do
+describe Puppet::Type.type(:linux_firewall) do
+
+  let :linux_firewall do
+    Puppet::Type.type(:linux_firewall).new(
+      :name => 'foo',
+      :source_ip => '1.2.3.4',
+      :dest_ip => '2.3.4.5',
+    )
+  end
+
+  describe "the ensure method" do
+    it "it should default ensure => present" do
+      linux_firewall[:ensure].should eq :present
+    end
+  end
+
+  describe "the name parameter" do
+    it do
+    end
+  end
+
+  describe "the source_ip parameter" do
+    it "should accept a cidr ip address" do
+      linux_firewall[:source_ip] = '1.2.3.4/32'
+      linux_firewall[:source_ip].should == '1.2.3.4/32'
+    end
+  end
+
+  describe "the dest_ip parameter" do
+    it "should accept a cidr ip address" do
+      linux_firewall[:source_ip] = '1.2.3.4/32'
+      linux_firewall[:source_ip].should == '1.2.3.4/32'
+    end
+  end
+
 end
