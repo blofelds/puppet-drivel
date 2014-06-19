526a54cd46b1dc94616c3ef16854cb6088c3cb53
GS <gs@github.com>
2014-06-20 00:20:57 +0100

    REFACTOR TEST TO GIVE AN EXPECTED ERROR MESSAGE
    this error message is much better (or just different) because it
    basically says that ruby can't find the iptables binary within its
    path. so if this test was run on a linux box that could find iptables
    in its path, the test would pass..
    right?
    
    error;
    
      Puppet::Error:
        Command iptables is missing
    
    putting a dummy executable file called 'iptables' somewhere in the path
    alters (fixes?) the error to a "more correct" 'unable to match regex'
    type of error.
    
    changes;
    
    * put the class declaration into a variable called 'provider_class'.
    
    * use a 'before :each' block inside a context instead of a 'let' block
      outside of a describe block.
    
    * use a 'context' block instead of a 'describe' block.
    
    * use '@provider = new linux_firewall' instead of 'let (:provider )'
    
    * call '@provider.destroy.should' instead of 'provider.destroy.should'

--- modules/firewall/spec/unit/puppet/provider/iptables_spec.rb
+++ modules/firewall/spec/unit/puppet/provider/iptables_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
-describe Puppet::Type.type(:linux_firewall).provider(:iptables) do
+provider_class = Puppet::Type.type(:linux_firewall).provider(:iptables)
+describe provider_class do
 
-  let(:resource) do
-    Puppet::Type.type(:linux_firewall).new(
-      :name => 'rule1',
-      :source_ip => '2.3.4.5/32',
-      :dest_ip => '3.4.5.6/32',
-    )
-  end
 
-  describe 'destroy' do
-  @resource =  Puppet::Type::Linux_firewall.new( {:name => 'rule1', :source_ip => '2.3.4.5/32', :dest_ip => '3.4.5.6/32'} )
-  let (:provider) { Puppet::Type.type(:linux_firewall).provider(:iptables).new(@resource)}
+  context "when destroying" do
 
-    it "destroys a rule that matches the resource instance's source, dest and name" do
-
-    provider.destroy.should == '-D INPUT -s 2.3.4.5/32 -d 3.4.5.6/32 -m comment --comment name -j REJECT'
-
+    before :each do
+      @resource =  Puppet::Type::Linux_firewall.new(
+        {:name => 'rule1', :source_ip => '2.3.4.5/32', :dest_ip => '3.4.5.6/32'}
+      )
+      @provider = provider_class.new(@resource)
     end
+
+       it "destroys a rule that matches the resource instance's source, dest and name" do
+
+       @provider.destroy.should == '-D INPUT -s 2.3.4.5/32 -d 3.4.5.6/32 -m comment --comment name -j REJECT'
+
+     end
+
   end
 
 end
